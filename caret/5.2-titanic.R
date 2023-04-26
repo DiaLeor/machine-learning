43112a331954ff2cc6445f83198d809aae0dc0d6
Dia Leor Paule <dia.paule@gmail.com>
2023-04-26 10:02:07 +0800

    Initial commit of Seciton 5

---
+++ caret/5.2-titanic.R
@@ -0,0 +1,8 @@
+## Section 5.2 - Titanic Exercises
+
+# Titanic Exercises Pt. 1 -------------------------------------------------
+
+
+# Titanic Exercises Pt. 2 -------------------------------------------------
+
+
