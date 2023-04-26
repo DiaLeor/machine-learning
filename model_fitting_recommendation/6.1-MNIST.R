ebdbbf854aeee8cfacb9f53108885cb948d5c948
Dia Leor Paule <dia.paule@gmail.com>
2023-04-26 10:02:14 +0800

    Initial commit of Seciton 6

---
+++ model_fitting_recommendation/6.1-MNIST.R
@@ -0,0 +1,17 @@
+## Section 6.1 - Case Study: MNIST
+
+# MNIST Case Study: Preprocessing -----------------------------------------
+
+
+# MNIST Case Study: kNN ---------------------------------------------------
+
+
+# MNIST Case Study: Random Forest -----------------------------------------
+
+
+# MNIST Case Study: Variable Impotance ------------------------------------
+
+
+# Ensembles ---------------------------------------------------------------
+
+
