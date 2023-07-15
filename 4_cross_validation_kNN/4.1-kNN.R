3f0dee61b1bc3cecc468af0f68849551e8a1289d
Dia Leor Paule <dia.paule@gmail.com>
2023-07-16 00:12:44 +0800

    Edit notes on k-Nearest Neighbors.

--- 4_cross_validation_kNN/4.1-kNN.R
+++ 4_cross_validation_kNN/4.1-kNN.R
@@ -5,28 +5,25 @@
 # K-nearest neighbors (kNN) estimates the conditional probabilities in a similar way to bin
 # smoothing. However, kNN is easier to adapt to multiple dimensions.
 
-# Using kNN, for any point (x_1,x_2) for which we want an estimate of p(x_1,x_2), we look for
-# the nearest points to (x_1,x_2) and take an average of the 0s and 1s associated with these
-# points. We refer to the set of points used to compute the average as the neighborhood.
+# Using kNN, for any point (x_1,x_2) for which we want an estimate of p*(x_1,x_2), we look
+# for the k nearest points to (x_1,x_2) and take an average of the 0s and 1s associated with
+# these points. We refer to the set of points used to compute the average as the neighborhood.
 # Larger values of k result in smoother estimates, while smaller values of k result in more
-# flexible and more wiggly estimates. 
+# flexible and more wiggly estimates.
 
-# To implement the algorithm, we can use the knn3() function from the caret package. There are
-# two ways to call this function:
-  
-  # 1. We can specify a formula and a data frame. The formula looks like
-  # y ~ x_1 + x_2 or y ~.
-  
-  # the predict() function for knn3 produces a probability for each class.
-  # 2. We can also call the function with the first argument being the matrix predictors and
-  # the second a vector of outcomes, like this:
+# To implement the algorithm, we can use the knn3() function from the caret package. There
+# are two ways to call this function:
+    # 1. We can specify a formula and a data frame. The formula looks like y ~ x_1 + x_2 or y~..
+    # The predict() function for knn3 produces a probability for each class.
 
+    # 2. We can also call the function with the first argument being the matrix predictors and
+    # the second a vector of outcomes, like this:
 x <- as.matrix(mnist_27$train)
 y <- mnist_27$train$y
 knn_fit <- knn3(x,y)
 
-# Overtraining is when a machine learning model can make good predictions on the training set
-# but cannot generalize well to new data. Overtraining is a reason why we have higher
+# Overtraining is when a machine learning model can make good predictions on the training
+# set but cannot generalize well to new data. Overtraining is a reason why we have higher
 # accuracy in the training set compared to the test set.
 
 # ..Code..
@@ -76,16 +73,17 @@ confusionMatrix(y_hat_knn, mnist_27$train$y)$overall["Accuracy"]
 y_hat_knn <- predict(knn_fit, mnist_27$test, type = "class")
 confusionMatrix(y_hat_knn, mnist_27$test$y)$overall["Accuracy"]
 
-# Overtraining and Oversmoothing ----------------------------------------
+# Overtraining and Oversmoothing ------------------------------------------
 
-# With k-nearest neighbors (kNN), overtraining is at its worst when we set k = 1.
-# With k = 1, the estimate for each (x_1,x_2) in the training set is obtained with just
-# the y corresponding to that point.
+# With k-nearest neighbors (kNN), overtraining is at its worst when we set k = 1. With k = 1,
+# the estimate for each (x_1,x_2) in the training set is obtained with just the y corresponding
+# to that point.
 
-# Oversmoothing can occur when when k is too large and does not permit enough flexibility.
+# Oversmoothing can occur when k is too large and does not permit enough flexibility.
 
-# The goal of cross validation is to estimate quantities such as accuracy or expected MSE so we
-# can pick the best set of tuning parameters for any given algorithm, such as the k for kNN.
+# The goal of cross validation is to estimate quantities such as accuracy or expected MSE
+# so we can pick the best set of tuning parameters for any given algorithm, such as the k
+# for kNN.
 
 # ..Code..
 knn_fit_1 <- knn3(y ~ ., data = mnist_27$train, k = 1)
